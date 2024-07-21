53fc57a6b813b38e20ebc3c9feef2b6dffc506d1
KostBoh <bohdan.kostya@gmail.com>
2024-07-21 12:10:20 +0300

    add change step2

--- src/index.js
+++ src/index.js
@@ -1,37 +1,87 @@
-import { program } from "commander";
-program
-  .option("-a, --action <type>", "choose action")
-  .option("-i, --id <type>", "user id")
-  .option("-n, --name <type>", "user name")
-  .option("-e, --email <type>", "user email")
-  .option("-p, --phone <type>", "user phone");
+// import { program } from "commander";
+// program
+//   .option("-a, --action <type>", "choose action")
+//   .option("-i, --id <type>", "user id")
+//   .option("-n, --name <type>", "user name")
+//   .option("-e, --email <type>", "user email")
+//   .option("-p, --phone <type>", "user phone");
 
-program.parse();
+// program.parse();
 
-const options = program.opts();
+// const options = program.opts();
 
-// TODO: рефакторити
-async function invokeAction({ action, id, name, email, phone }) {
+// // TODO: рефакторити
+// async function invokeAction({ action, id, name, email, phone }) {
+//   switch (action) {
+//     case "list":
+//       // ...
+//       break;
+
+//     case "get":
+//       // ... id
+//       break;
+
+//     case "add":
+//       // ... name email phone
+//       break;
+
+//     case "remove":
+//       // ... id
+//       break;
+
+//     default:
+//       console.warn("\x1B[31m Unknown action type!");
+//   }
+// }
+
+// invokeAction(options);
+
+// console.log("Hello Node");
+
+import {
+  listContacts,
+  getContactById,
+  addContact,
+  updateById,
+  removeContact,
+} from "./contacts.js";
+
+const invokeAction = async ({ action, id, ...data }) => {
   switch (action) {
     case "list":
-      // ...
-      break;
-
+      const list = await listContacts();
+      return console.log(list);
     case "get":
-      // ... id
-      break;
-
+      const oneContact = await getContactById(id);
+      return console.log(oneContact);
     case "add":
-      // ... name email phone
-      break;
-
-    case "remove":
-      // ... id
-      break;
+      const newContact = await addContact(data);
+      return console.log(newContact);
+    case "updateById":
+      const updateContact = await updateById(id, data);
+      return console.log(updateContact);
+    case "removeContact":
+      const deleteContact = await removeContact(id);
+      return console.log(deleteContact);
 
     default:
-      console.warn("\x1B[31m Unknown action type!");
+      console.log("Unknow action");
   }
-}
-
-invokeAction(options);
+};
+// invokeAction({ action: "list" });
+// invokeAction({ action: "get", id: "AeHIrLTr6JkxGE6SN-0Rw" });
+// invokeAction({
+//   action: "add",
+//   name: "Kostya Bohdan",
+//   email: "1234@gmail.com",
+//   phone: "+3805555555",
+// });
+// invokeAction({
+//   action: "updateById",
+//   id: "mdFYyiDs2sGRjBA0r0SAf",
+//   phone: "+3809999999",
+// });
+invokeAction({
+  action: "removeContact",
+  id: "mdFYyiDs2sGRjBA0r0SAf",
+});
